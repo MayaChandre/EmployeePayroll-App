b92cc58d923d1e4893cb6716956361a4ca9a5b3a
mayavathi <mayachandre27@gmail.com>
2021-08-16 06:06:07 -0400

    Added:Ability to update an employee payroll intlo local storage

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,62 +1,64 @@
-class EmployeePayrollData{
-    get id(){
-        return this._id;
+class EmployeePayrollData {
+    
+    //getter and setter method
+    get id() {return this._id;}
+    set id(id) {
+        this._id=id;
     }
-    set id(id){
-        this._id = id;
-    }
-    get name(){
+
+    get name(){ 
         return this._name;
     }
     set name(name) {
-        let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
-        if (nameRegex.test(name))
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameRegex.test(name))
             this._name = name;
-        else
-            throw 'Name is Invalid!'
+        else throw 'Name is Incorrect!';
     }
-    get profilePic(){
-        return this._profilePic;
-    }
+
+    get profilePic() { return this._profilePic;}
     set profilePic(profilePic){
         this._profilePic = profilePic;
     }
-    get gender(){
-        return this._gender;
-    }
-    set gender(gender){
+
+    get gender() { return this._gender; }
+    set gender(gender) {
         this._gender = gender;
     }
-    get department(){
-        return this._department;
-    }
+
+    get department() { return this._department; }
     set department(department){
         this._department = department;
     }
-    get salary(){
-        return this._salary;
-    }
+
+    get salary() { return this._salary; }
     set salary(salary){
-        this._salary=salary;
-    }
-    get notes(){
-        return this._notes;
-    }
-    set notes(notes){
-        this._notes = notes;
-    }
-    get startDate(){
-        return this._startDate;
-    }
-    set startDate(startDate){
-        if (startDate <= new Date())
-            this._startDate = startDate;
-        else
-            throw 'StartDate is Invalid!';
+        this._salary = salary;
     }
 
-    toString() {
-        const empDate = stringifyDate(this.startDate);
-        return "id = "+this.id+", name = " + this.name + ", profilePic = " + this.profilePic + ", salary = " + this.salary + ", gender = " + this.gender + ", department = " + this.department + ", startDate = " + empDate + ", notes= " + this.notes;
+    get note() { return this._note; }
+    set note(note) {
+        this._note = note;
+    }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now)
+            throw 'Start Date is a future Date!';
+        var diff = Math.abs(now.getTime()-startDate.getTime());
+        if (diff/(1000*60*60*24) > 30)
+            throw 'Start Date is beyond 30 Days!';
+        this._startDate = startDate;
+    }
+
+    toString(){
+        const options = { year:'numeric',month:'long',day:'numeric'}
+        const empDate = !this.startDate ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US",options);
+        return "id ="+this.id+", name ="+this.name+", gender = "+this.gender+", profilePic = "+this.profilePic+
+                ", department = "+this.department+", salary = "+this.salary+", gender = "+this.gender+
+                ", startDate = "+empDate+", note = "+this.note;
     }
 }
+
